ec1c3ba49dabfba924dbadae556db8fda86ac336
clover <tclover@tutanota.com>
2020-02-29 19:57:02 +0100

    initial

---
+++ src/main/java/com/weavertest/javafxweaverexample/WeatherService.java
@@ -0,0 +1,11 @@
+package com.weavertest.javafxweaverexample;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+    public String getWeatherForecast() {
+        return "Winter is coming.";
+    }
+}
